032365632c10701291dbc62c03177b7331397ada
lindzeng <lindyzeng.lz@gmail.com>
2016-04-03 01:26:13 -0400

    Updatessssssssssss 1:30 AM

--- app/src/main/java/com/example/lindyzeng/munchwithme/LoginActivity.java
+++ app/src/main/java/com/example/lindyzeng/munchwithme/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.lindyzeng.munchwithme;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -26,6 +34,48 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
                 LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+        public void onClick(View v) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if (success) {
+                                String name = jsonResponse.getString("name");
+                                String username = jsonResponse.getString("username");
+
+                                Intent intent = new Intent(LoginActivity.this, UserAreaActivity.class);
+                                intent.putExtra("name", name);
+                                intent.putExtra("username", username);
+
+                                LoginActivity.this.startActivity(intent);
+                            }
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("Login failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
             }
         });
     }
--- app/src/main/java/com/example/lindyzeng/munchwithme/UserAreaActivity.java
+++ app/src/main/java/com/example/lindyzeng/munchwithme/UserAreaActivity.java
@@ -1,19 +1,63 @@
 package com.example.lindyzeng.munchwithme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import java.io.InputStream;
+import java.net.HttpCookie;
+
 
 public class UserAreaActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
 
-        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
-        final EditText etName = (EditText) findViewById(R.id.etName);
-        final TextView welcomeMessage = (TextView) findViewById(R.id.tvWelcomeMsg);
+        // final Button bCreateEvent = (Button) findViewById(R.id.bCreateEvent);
+        //  bCreateEvent.setOnClickListener(new View.OnClickListener() {
+        //   @Override
+        // public void onClick(View v) {
+        // Intent createEventIntent = new Intent(UserAreaActivity.this, CreateEvent.class);
+        // UserAreaActivity.this.startActivity(createEventIntent);
+        // }
+        // });
+
+        String[] names = {"Lindy", "Luisa", "Jessica"};
+
+        ListView listv = (ListView) findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.layout_userarea, R.id.name, names);
+        listv.setAdapter(adapter);
+        listv.setOnItemClickListener(new ItemList());
     }
+
+    class ItemList implements AdapterView.OnItemClickListener {
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            ViewGroup vg = (ViewGroup) view;
+            TextView tv = (TextView) vg.findViewById(R.id.name);
+            Toast.makeText(UserAreaActivity.this, tv.getText().toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    String result = null;
+    InputStream is = null;
+
+    // Intent intent = getIntent();
+    // String name = intent.getStringExtra("name");
+    // String username = intent.getStringExtra("username");
+
 }
